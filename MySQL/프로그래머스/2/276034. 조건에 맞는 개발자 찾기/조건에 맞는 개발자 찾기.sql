19134d397029cf503f9f777c680c7d31b5a40e0a
HwangHyeonseok <51507287+HwangHyeonseok@users.noreply.github.com>
2025-04-04 22:11:22 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ MySQL/프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -0,0 +1,28 @@
+# 비트 연산(&) + join
+
+# SELECT * FROM SKILLCODES -- 개발자가 사용하는 프로그래밍 언어 정보
+# SELECT * FROM DEVELOPERS -- 개발자들의 프로그래밍 스킬 정보
+
+-- 400 이 256(Python) 이나 1024(C#) 스킬을 가지고 있는지 확인하려면, 
+-- 400(110010000) & 256(100000000) == 256(100000000)
+-- 400(110010000) & 1024(0011001000) != 1024
+
+-- 파이썬 CODE 번호 가져오기
+with python_code_info as
+(
+    SELECT *
+    FROM SKILLCODES
+    WHERE NAME = 'Python'
+),
+-- C# CODE 번호 가져오기
+csharp_code_info as
+(
+    SELECT *
+    FROM SKILLCODES
+    WHERE NAME = 'C#'
+)
+
+SELECT distinct d.ID, d.EMAIL, d.FIRST_NAME, d.LAST_NAME
+FROM DEVELOPERS d left join SKILLCODES s ON d.SKILL_CODE & s.CODE = (SELECT CODE FROM python_code_info) or d.SKILL_CODE & s.CODE = (SELECT CODE FROM csharp_code_info)
+WHERE s.NAME is not null
+ORDER BY 1 asc
