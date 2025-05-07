34afe245b6bb618758767a612b60079ae356840e
HwangHyeonseok <51507287+HwangHyeonseok@users.noreply.github.com>
2025-05-08 00:41:42 +0900

    [level 0] Title: 순서쌍의 개수, Time: 0.05 ms, Memory: 84.1 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+            for(int i=1; i<=Math.sqrt(n); i++) {
+                if(n%i == 0) {
+                    answer+=2;
+                }
+            }
+        
+        if(Math.sqrt(n) % 1 == 0) answer--;
+        return answer;
+    }
+}
