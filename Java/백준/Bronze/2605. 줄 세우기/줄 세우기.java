fee784dec2ad70260ea22e3ac96121d230efa629
HwangHyeonseok <51507287+HwangHyeonseok@users.noreply.github.com>
2025-05-23 12:59:41 +0900

    [Bronze II] Title: 줄 세우기, Time: 116 ms, Memory: 15760 KB -BaekjoonHub

--- Java/백준/Bronze/2605. 줄 세우기/줄 세우기.java
+++ Java/백준/Bronze/2605. 줄 세우기/줄 세우기.java
@@ -2,44 +2,31 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	static ArrayList<Integer> list = new ArrayList<>();
-	static ArrayList<Integer> pickNumList = new ArrayList<>();
-	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
 		// input //
-		int studentCnt = Integer.parseInt(br.readLine()); // 학생 수
+		int studentCnt = Integer.parseInt(br.readLine());
+		int[] pickNum = new int[studentCnt]; // 뽑은 번호
+		
 		String input = br.readLine();
 		StringTokenizer st = new StringTokenizer(input, " ");
 		for(int i=0; i<studentCnt; i++) {
-			pickNumList.add(Integer.parseInt(st.nextToken()));
+			pickNum[i] = Integer.parseInt(st.nextToken());
 		}
 		
 		// logic //
-		line(studentCnt);
+		// i-pickNum[i] 인덱스에 값 삽입
+		ArrayList<Integer> studentLine = new ArrayList<>(); // 학생이 줄 선 현황을 저장
+		for(int i=0; i<studentCnt; i++) {
+			studentLine.add(i-pickNum[i], i+1);
+		}
 		
 		// output //
-		for(int i : list) {
-			bw.write(i + " ");
+		for(int ans : studentLine) {
+			bw.write(ans+" ");
 		}
 		bw.flush();
 		bw.close();
-	}
-	
-	// 줄 서기 로직 //
-	public static void line(int studentCnt) {
-		for(int i=1; i<=studentCnt; i++) {
-			// 첫 번째 학생인 경우
-			if(i==1) {
-				list.add(1);
-				continue;
-			}
-			// 2~N번째 학생까지는 (i-1)-pickNumList.get(i-1) 인덱스에 i 값 추가
-			else {
-				list.add((i-1)-pickNumList.get(i-1),i);
-			}
-		}
 	}
 }
