d30c15eababecd36aa1132c68224497132a70660
HwangHyeonseok <51507287+HwangHyeonseok@users.noreply.github.com>
2026-02-13 21:15:37 +0900

    [level 2] Title: 연도별 대장균 크기의 편차 구하기, Time: , Memory: undefined -BaekjoonHub

--- MySQL/프로그래머스/2/299310. 연도별 대장균 크기의 편차 구하기/연도별 대장균 크기의 편차 구하기.sql
+++ MySQL/프로그래머스/2/299310. 연도별 대장균 크기의 편차 구하기/연도별 대장균 크기의 편차 구하기.sql
@@ -1,31 +1,18 @@
-# 방법 1 : 단순 서브쿼리 이용
+-- 연도별 가장 큰 대장균의 크기
+# SELECT YEAR(DIFFERENTIATION_DATE), MAX(SIZE_OF_COLONY) as 'MAX_SIZE_OF_COLONY'
+# FROM ECOLI_DATA
+# GROUP BY YEAR(DIFFERENTIATION_DATE)
 
-# -- select : 연도, 크기 편차(가장 큰 대장균의 크기 - 각 대장균의 크기), 대장균 개체 ID
-
-# -- 연도별 가장 큰 대장균의 크기 구하기 
-# with large_size_colony as
-# (
-#     SELECT YEAR(DIFFERENTIATION_DATE) as 'year', max(SIZE_OF_COLONY) as 'max_size' 
-#     FROM ecoli_data
-#     GROUP BY YEAR(DIFFERENTIATION_DATE)
-# ),
-# -- ecoli_data 테이블에서 원하는 것만 수정
-# abstract_ecoli_data as
-# (
-#     SELECT ID, YEAR(DIFFERENTIATION_DATE) as 'year',SIZE_OF_COLONY
-#     FROM ecoli_data
-# )
-
-# SELECT e.YEAR, max_size - size_of_colony as YEAR_DEV,ID
-# FROM abstract_ecoli_data e LEFT JOIN large_size_colony l ON e.year=l.year
-# ORDER BY 1 asc, 2 asc
-
-# 방법 2 : 윈도우 함수 이용 (집계도 필요하고, 테이블의 값도 이용해야 하고 그룹화도 필요하므로.)
-# -> 방법 1은 스캔을 2번 해야 하는데, 여기에서는 한 번에 해서 성능이 더 좋을 것 같음
-
-SELECT
-    YEAR(differentiation_date) as 'YEAR',
-    MAX(SIZE_OF_COLONY) OVER (PARTITION BY year(differentiation_date)) - size_of_colony as 'YEAR_DEV',
+SELECT 
+    YEAR(DIFFERENTIATION_DATE) AS 'YEAR',
+    MAX_SIZE_OF_COLONY - SIZE_OF_COLONY AS 'YEAR_DEV',
     ID
-FROM ECOLI_DATA
-ORDER BY 1 asc, 2 asc
+FROM ECOLI_DATA e1
+    LEFT JOIN (
+        -- 연도별 가장 큰 대장균의 크기 (서브쿼리)
+        SELECT YEAR(e.DIFFERENTIATION_DATE) as 'YEAR', MAX(e.SIZE_OF_COLONY) as 'MAX_SIZE_OF_COLONY'
+        FROM ECOLI_DATA e
+        GROUP BY YEAR(e.DIFFERENTIATION_DATE)
+    ) e2
+    ON YEAR(e1.DIFFERENTIATION_DATE) = e2.YEAR
+ORDER BY YEAR asc, YEAR_DEV asc
